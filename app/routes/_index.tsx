ec6afcdff43b32ee070a17d693a567b313b9d03d
Dadit <dadit.utama@gmail.com>
2024-09-15 12:38:44 +0700

    Home (fix): adding abstraction for skills

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import { CertificateWrapper } from "~/components/card.certificate";
-import { Input } from "~/components/input";
-import { Textarea } from "~/components/textarea";
-import { Button } from "~/components/button";
-import { languages, technologies } from "~/data/data";
+import { CertificateWrapper } from "~/components/certificate";
+import { Skills } from "~/components/skills";
 
 export const meta: MetaFunction = () => {
   return [
@@ -17,10 +14,10 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <>
+    <div>
       <div className="flex flex-col-reverse md:flex-row md:gap-24 justify-center gap-10 items-center border-t border-b border-zinc-700 h-[400px]">
         <div className="text-center md:text-right md:scale-125">
-          <h5>A Fullstack Web Developer</h5>
+          <h5>A Software Developer</h5>
           <h1 className="">Farid Firda Utama</h1>
         </div>
         <div className="flex flex-col justify-center items-center h-[108px] w-[108px] bg-teal-500 rounded-full md:scale-125">
@@ -37,9 +34,9 @@ export default function Index() {
       <div className="flex flex-col gap-5 mt-10 mb-10 p-10">
         <h3>About Me</h3>
         <p className="dark:text-zinc-400">
-          A Fullstack Web Developer whose passion lies in creating a good
-          quality code and software for humans, including but not limited to
-          design and product development.
+          A Software Developer whose passion lies in creating a good quality
+          code and software for humans, including but not limited to design and
+          product development (fulstack web and ios app).
         </p>
         <p className="dark:text-zinc-400">
           As a self-taught learner, I passioned to learn a new technology
@@ -57,49 +54,8 @@ export default function Index() {
 
         <h6>Certificates :</h6>
         <CertificateWrapper />
-
-        <h3 className="mt-5">Skills</h3>
-        <h6 className="dark:text-zinc-400">Programming Languages :</h6>
-        <div className="flex flex-row gap-2">
-          {languages.map((language, index) => {
-            return (
-              <div
-                key={index}
-                className="border border-teal-600 rounded-lg px-3 py-1 text-teal-500 hover:bg-black dark:hover:bg-teal-600 hover:text-teal-300 dark:hover:text-white"
-              >
-                {language}
-              </div>
-            );
-          })}
-        </div>
-
-        <h6 className="dark:text-zinc-400">Technologies :</h6>
-        <div className="flex flex-row flex-wrap gap-2">
-          {technologies.map((technology, index) => {
-            return (
-              <div
-                key={index}
-                className="border border-teal-600 rounded-lg px-3 py-1 text-teal-500 hover:bg-black dark:hover:bg-teal-600 hover:text-teal-300 dark:hover:text-white"
-              >
-                {technology}
-              </div>
-            );
-          })}
-        </div>
+        <Skills />
       </div>
-
-      {/* CONTACT ME */}
-      <div className="flex flex-col gap-5 p-10">
-        <h3>Contact Me</h3>
-        <form className="flex flex-col gap-5 md:max-w-[600px]">
-          <Input placeholder="Enter your name" />
-          <Input placeholder="Enter your email" />
-          <Textarea placeholder="Enter your message" rows={5} />
-          <Button variant="primaryGr" size="md">
-            Submit
-          </Button>
-        </form>
-      </div>
-    </>
+    </div>
   );
 }
