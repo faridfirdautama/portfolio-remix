b59d94a1f78d4c850969f640160225b2cca4752b
Dadit <dadit.utama@gmail.com>
2024-09-16 08:11:52 +0700

    Logo (feat): adding logo for projects and uses

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -8,10 +8,18 @@ export const meta: MetaFunction = () => {
 export default function Projects() {
   return (
     <main>
-      <div className="flex flex-col-reverse md:flex-row justify-center gap-10 items-center border-t border-b border-zinc-700 h-[250px] md:h-[400px]">
-        <div className="text-center md:scale-125">
+      <div className="flex flex-col-reverse md:flex-row justify-center gap-10 items-center border-t border-b border-zinc-700 h-[350px] md:h-[400px]">
+        <div className="text-center md:scale-125 md:text-right md:mr-10">
           <h5>Right now...</h5>
           <h1>What I am building</h1>
+        </div>
+        <div className="flex flex-col justify-center items-center h-[108px] w-[108px] bg-teal-500 rounded-full md:scale-125">
+          <img
+            width={100}
+            className="rounded-full "
+            src="/code.png"
+            alt="gear"
+          />
         </div>
       </div>
       <ProjectWrapper />
